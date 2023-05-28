477f6edb14cb02e5c0f7581e590d95601e70ba1f
Simone Delle Fave <simonedellefave21@gmail.com>
2023-05-29 00:09:40 +0200

    feat: added brand url

--- .storybook/myTheme.js
+++ .storybook/myTheme.js
@@ -1,8 +1,9 @@
-import { create } from '@storybook/theming';
+import {create} from '@storybook/theming';
 
 export default create({
     base: 'light',
     brandTitle: 'Eden Storybook',
+    brandUrl: 'https://github.com/simone-frontdev/eden-design-system',
     brandImage: 'https://i.ibb.co/Mg48fKG/New-Project-24.png',
     brandTarget: '_self',
 });
